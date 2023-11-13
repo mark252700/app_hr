ef7e81433836b23ae998fee541408d02a55b28bd
snjhyrglr <jayjregalario@gmail.com>
2023-11-13 11:04:20 +0800

    update prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '103.3.62.16', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
